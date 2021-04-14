20be2eaefa54c272422c583e1622d2c02fe1b9d9
lihongwei <lihongwei@nucarf.com>
2021-04-14 17:32:25 +0800

    log

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,18 @@
 <?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+if (! function_exists('warning')) {
+
+    function warning($message = '', array $headers = [])
+    {
+        // create a log channel
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler(storage_path('logs/lumendd.log'), Logger::WARNING));
+
+        // add records to the log
+        $log->warning('Foo');
+        $log->error('Bar');
+    }
+}
