488c3dfb3ad82d154b4ba573516ad65f300f2d7f
lihongwei <lihongwei@nucarf.com>
2021-04-26 22:40:25 +0800

    log

--- src/functions.php
+++ src/functions.php
@@ -5,14 +5,88 @@ use Monolog\Handler\StreamHandler;
 
 if (! function_exists('warning')) {
 
-    function warning($message = '', array $headers = [])
+    function warning($message, array $context = [])
     {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
         // create a log channel
         $log = new Logger($message);
-        $log->pushHandler(new StreamHandler(storage_path('logs/lumendd.log'), Logger::WARNING));
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
 
         // add records to the log
-        $log->warning('Foo');
-        $log->error('Bar');
+        $log->warning($message, $context);
+    }
+}
+
+
+if (! function_exists('error')) {
+    function error($message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->error($message, $context);
+    }
+}
+
+
+
+if (! function_exists('emergency')) {
+    function emergency($message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->emergency($message, $context);
+    }
+}
+
+
+
+if (! function_exists('notice')) {
+    function notice($message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->notice($message, $context);
+    }
+}
+
+
+if (! function_exists('info')) {
+    function info($message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->info($message, $context);
+    }
+}
+
+if (! function_exists('debug')) {
+    function debug($message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->debug($message, $context);
+    }
+}
+
+
+if (! function_exists('blog')) {
+    function blog($level,$message, array $context = [])
+    {
+        $fileName = 'store-service-'.date('Y-m-d').'.log';
+        $file = storage_path('logs/'.$fileName);
+        $log = new Logger($message);
+        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->log($level,$message, $context);
     }
 }
